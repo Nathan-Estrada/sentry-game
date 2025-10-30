037898e30c51014b6e9004d33462143b8044f035
Nathan <nathan000estrada@gmail.com>
2025-10-30 06:03:27 -0500

    Added textures and animations for sentry and npc, fixed sentry movement

---
+++ npc.gd
@@ -0,0 +1,17 @@
+extends CharacterBody3D
+
+@onready var animation = $Pivot/Zombie/AnimationPlayer
+const SPEED = 3
+const JUMP_VELOCITY = 4.5
+
+func _ready():
+	velocity = Vector3.FORWARD * -SPEED
+	
+	
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	animation.play("Armature|Walk")
+	move_and_slide()
+	
--- sentry.gd
+++ sentry.gd
@@ -1,17 +1,16 @@
 extends Node3D
 
-const turn_speed = 0.1
+var turn_speed = 0.4
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	$Timer.start()
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if (rotation_degrees.y >= 180):
-		rotate_y(deg_to_rad(-turn_speed))
-	elif (rotation_degrees.y <= 50):
-		rotate_y(deg_to_rad(-turn_speed))
-	print(rotation_degrees.y)
-		
+	rotate_y(deg_to_rad(turn_speed))
+
+
+func _on_timer_timeout() -> void:
+	turn_speed = -turn_speed
